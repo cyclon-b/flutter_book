8b909b9519775b739f5b202c55e6505c1373d477
Gennady Artamonov <gartamonov@clink.tech>
2025-03-27 23:24:06 +0300

    basic structure WIP

--- lib/screens/notes/notes_model.dart
+++ lib/screens/notes/notes_model.dart
@@ -1,11 +1,43 @@
-import '../../store/base_classes/base_app_model.dart';
+import 'dart:convert';
 
-class NoteModel {
-  late int id;
+import '../../store/base_classes/base_app_model.dart';
+import '../../store/base_classes/base_entity_model.dart';
+
+class NoteModel implements BaseEntityModel {
+  late String id;
   late String title;
   late String content;
   late String color;
 
+  NoteModel({
+    required this.id,
+    required this.title,
+    required this.color,
+    required this.content,
+  });
+
+  @override
+  fromJson(String jsonString) {
+    Map<String, dynamic> jsonInMap = jsonDecode(jsonString);
+    return NoteModel(
+        id: jsonInMap['id'],
+        title: jsonInMap['title'],
+        color: jsonInMap['color'],
+        content: jsonInMap['content']);
+  }
+
+  @override
+  String modelToJson() {
+    Map<String, dynamic> resultMap = {
+      id: id,
+      title: title,
+      content: content,
+      color: color,
+    };
+    return jsonEncode(resultMap);
+  }
+
+  @override
   String toString() {
     return "{id = $id, title = $title, content = $content, color = $color} ";
   }
@@ -19,3 +51,5 @@ class NotesModel extends BaseAppModel {
     notifyListeners();
   }
 }
+
+final NotesModel notesModel = NotesModel();
---
+++ lib/store/base_classes/base_entity_model.dart
@@ -0,0 +1,5 @@
+class BaseEntityModel {
+  fromJson(String json) {}
+
+  modelToJson() {}
+}
